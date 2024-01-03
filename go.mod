c7fdc1e168b8a4aa9e1f8df6bd3b45a84e802780
usysrc <usysrc@usysrc.dev>
2024-01-03 08:15:46 +0100

    added noise generation

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/usysrc/noise
+
+go 1.21.4
+
+require github.com/gen2brain/raylib-go/raylib v0.0.0-20231230150416-17ce08145200
+
+require (
+	github.com/ebitengine/purego v0.6.0-alpha.2 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"math/rand"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+func main() {
+	rl.InitWindow(800, 600, "noise")
+	defer rl.CloseWindow()
+
+	rl.SetTargetFPS(60)
+	var cols [800 * 600]rl.Color
+	for i := 0; i < 800; i++ {
+		for j := 0; j < 600; j++ {
+			cols[j*600+i] = rl.Black
+			if rand.Intn(100) < 50 {
+				cols[j*600+i] = rl.White
+			}
+		}
+	}
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.RayWhite)
+		for i := 0; i < 800; i++ {
+			for j := 0; j < 600; j++ {
+				rl.DrawRectangle(int32(i), int32(j), 1, 1, cols[j*600+i])
+			}
+		}
+
+		rl.EndDrawing()
+	}
+}
